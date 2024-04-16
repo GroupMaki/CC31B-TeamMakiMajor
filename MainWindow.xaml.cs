b52805c48195b8f9b00d7959fbaeae1685594a3c
JUSTINE MAE MACARIO <jbm1737@students.uc-bcf.edu.ph>
2024-04-17 07:23:31 +0800

    Up till 12.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,45 +12,50 @@ using MajorEngine.ViewModels;
 using MajorEngine.Models;
 using MajorEngine.EventArgs;
 
-namespace CC31B_TeamMakiMajor;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
-{
-    private GameSession _gameSession;
-    public MainWindow()
+namespace CC31B_TeamMakiMajor
+{   /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        _gameSession = new GameSession();
-        DataContext = _gameSession;
-
+        private readonly GameSession _gameSession = new GameSession();
+        public MainWindow()
+        {
+            InitializeComponent();
+            _gameSession.OnMessageRaised += OnGameMessageRaised;
+            DataContext = _gameSession;
+        }
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
+        private void OnClick_AttackMonster(object sender, RoutedEventArgs e)
+        {
+            _gameSession.AttackCurrentMonster();
+        }
+        private void OnGameMessageRaised(object sender, GameMessageEventArgs e)
+        {
+            GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
+            GameMessages.ScrollToEnd();
+        }
+        private void OnClick_DisplayTradeScreen(object sender, RoutedEventArgs e)
+        {
+            TradeScreen tradeScreen = new TradeScreen();
+            tradeScreen.Owner = this;
+            tradeScreen.DataContext = _gameSession;
+            tradeScreen.ShowDialog();
+        }
     }
-    private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
-    {
-        _gameSession.MoveNorth();
-    }
-
-    private void OnClick_MoveEast(object sender, RoutedEventArgs e)
-    {
-        _gameSession.MoveEast();
-    }
-
-    private void OnClick_MoveWest(object sender, RoutedEventArgs e)
-    {
-        _gameSession.MoveWest();
-    }
-
-    private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
-    {
-        _gameSession.MoveSouth();
-    }
-    private void OnGameMessageRaised(object sender , GameMessageEventArgs e)
-    {
-        GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
-        GameMessages.ScrollToEnd();
-
-    }
-
 }
